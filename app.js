0959ad08582c026d7f540d34cc9111989443f0df
Muhammad Osama Khan <khanosama1996@gmail.com>
2019-08-27 11:52:49 -0700

    cart design , js variables and classes

---
+++ app.js
@@ -0,0 +1,34 @@
+const cartBtn = document.querySelector('.cart-btn');
+const closeCartBtn = document.querySelector('.close-cart');
+const clearCartBtn = document.querySelector('.clear-cart');
+const cartDOM = document.querySelector('.cart');
+const cartOverlay = document.querySelector('.cart-overlay');
+const cartItems = document.querySelector('.cart-items');
+const cartTotal = document.querySelector('.cart-total');
+const cartContent = document.querySelector('.cart-content');
+const productDOM = document.querySelector('.products-center');
+
+//cart
+let cart = [];
+
+
+//getting the products
+class Products{
+
+}
+
+//display products
+class UI{
+
+}
+
+//local storage
+class Storage{
+
+}
+
+document.addEventListener("DOMContentLoader",()=>{
+    const ui =new UI();
+    const products = new Products();
+})
+
