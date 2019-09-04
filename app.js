20c3abec303bcfdf902f643d3b12e7fbb2e1f49f
Muhammad Osama Khan <khanosama1996@gmail.com>
2019-09-04 07:29:08 -0700

    getting products

--- app.js
+++ app.js
@@ -18,7 +18,15 @@ async getProducts(){
     try {
         let result = await fetch('products.json')
         let data = await result.json();
-        return data;
+        let products = data.items;
+        
+        products = products.map(item => {
+            const {title,price} = item.fields;
+            const id = item.sys;
+            const image = item.fields.image.fields.file.url;
+            return {title,price,id,image};
+        })
+        return products;
         
     } catch (error) {
         console.log(errors);
